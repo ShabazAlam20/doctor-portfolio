ca53d7afee95b79b11adba00ed7f02ac7959bcec
Shabaz Alam <shabazalam20@gmail.com>
2024-02-16 21:30:51 +0400

    changes

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -33,11 +33,11 @@ function SocialLinks() {
                 </>
             ),
             href: 'mailto:foo@gmail.com',
-            style: 'rounded-tr-md'
+            style: 'rounded-br-md'
         },
     ]
     return (
-        <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
+        <div className='flex flex-col top-[15%] lg:top-[35%] left-0 fixed'>
             <ul>
                 {links.map(({ id, child, href, style, download }) => (
                     <li key={id} className={"flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" + " " + style}>
